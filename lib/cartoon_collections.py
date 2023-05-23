7dda8904b7ca2ed8d133c3ecd889f7e45eb057da
River Ferguson <riverferguson@gmail.com>
2023-05-23 10:10:37 -0700

    complete lab

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,24 @@
-def roll_call_dwarves():
-    pass
+CHEESE = ["cheddar", "gouda", "camembert"]
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    i = 1
+    for name in dwarves:
+        print(f'{i}. {name}')
+        i += 1
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planteer_calls):
+    return [f'{call.title()}!' for call in planteer_calls]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+    for call in calls:
+        if len(call) > 4:
+            return True
+    return False
+
+def find_the_cheese(snacks):
+    for snack in snacks:
+        if snack in CHEESE:
+            return snack
+    return None
+    
+    
